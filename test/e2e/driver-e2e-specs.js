d5a20a255ff0437515a0b5f453e5b87b8f3516ac
Mykola Mokhnach <mokhnach@gmail.com>
2020-04-04 17:52:53 +0200

    refactor: Do not require hashsum match after driver download is completed (#76)

--- test/e2e/driver-e2e-specs.js
+++ test/e2e/driver-e2e-specs.js
@@ -2,36 +2,51 @@ import wd from 'wd';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { startServer } from '../../lib/server';
+import { isAdmin } from '../../lib/installer';
 chai.should();
 chai.use(chaiAsPromised);
 
 const TEST_PORT = 4788;
 const TEST_HOST = 'localhost';
 
-let server, driver;
+describe('Driver', async function () {
+  if (!await isAdmin()) {
+    return;
+  }
 
-describe('Driver', function () {
+  let server;
+  let driver;
+
   before(async function () {
-    server = await startServer(TEST_PORT, TEST_HOST);
+    if (await isAdmin()) {
+      server = await startServer(TEST_PORT, TEST_HOST);
+    }
   });
 
   after(async function () {
-    await server.close();
+    if (server) {
+      await server.close();
+    }
+    server = null;
   });
 
   beforeEach(function () {
-    driver = wd.promiseChainRemote(TEST_HOST, TEST_PORT);
+    if (server) {
+      driver = wd.promiseChainRemote(TEST_HOST, TEST_PORT);
+    }
   });
 
   afterEach(async function () {
-    await driver.quit();
+    if (driver) {
+      await driver.quit();
+    }
+    driver = null;
   });
 
   it('should run a basic session using a real client', async function () {
     await driver.init({
       app: 'Microsoft.WindowsCalculator_8wekyb3d8bbwe!App',
       platformName: 'Windows',
-      deviceName: 'WindowsPC'
     });
     await driver.elementByName('Calculator');
   });
